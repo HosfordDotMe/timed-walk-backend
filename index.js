3ea391377ae58a6e6d16de450f8422621392a6ce
Eric Hosford <eric.hosford@gmail.com>
2019-10-07 00:27:16 -0400

    Fix typo for keys in production

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-const keys = require('./config/dev');
+const keys = require('./config/keys');
 require('./models/test')
 const app = express();
 
